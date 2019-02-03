7efaf75039c0dcf471f032d397f9e27867f872bd
Illia Derhun <illya.dergum@gmail.com>
2019-02-03 18:16:55 +0200

    Package 36 finished

--- src/main/java/com/github/illiaderhun/aopdemo/MainDemoApp.java
+++ src/main/java/com/github/illiaderhun/aopdemo/MainDemoApp.java
@@ -2,6 +2,7 @@ package com.github.illiaderhun.aopdemo;
 
 import com.github.illiaderhun.DemoConfig;
 import com.github.illiaderhun.aopdemo.dao.AccountDAO;
+import com.github.illiaderhun.aopdemo.dao.MembershipDAO;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MainDemoApp {
@@ -10,9 +11,14 @@ public class MainDemoApp {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DemoConfig.class);
 
         AccountDAO accountDAO = context.getBean("accountDAO", AccountDAO.class);
+        MembershipDAO membershipDAO = context.getBean("membershipDAO", MembershipDAO.class);
 
         accountDAO.addAccount();
-        accountDAO.addAccount();
+
+        System.out.println("add account into membership");
+
+        membershipDAO.addAccount();
+        membershipDAO.addSillyMember();
 
         context.close();
     }
--- src/main/java/com/github/illiaderhun/aopdemo/aspect/LoggingAspect.java
+++ src/main/java/com/github/illiaderhun/aopdemo/aspect/LoggingAspect.java
@@ -8,9 +8,26 @@ import org.springframework.stereotype.Component;
 @Component
 public class LoggingAspect {
 
+    /**
+     * execution(modifiers-pattern?
+     *          return-type-pattern declaring-type-pattern?
+     *          method-name-pattern(param-pattern) throw-pattern?)
+     */
+
+    @Before("execution(public void com.github.illiaderhun.aopdemo.dao.AccountDAO.addAccount())")
+    public void beforeCertainAddAccountAdvice() {
+
+        System.out.println("\n======>>> Specifically @Before");
+    }
+
     @Before("execution(public void addAccount())")
-    public void beforeAddAccountAdvice() {
+    public void beforeAllAddAccountAdvice() {
 
         System.out.println("\n======>>> Executing @Before advice on addAccount()");
+    }
+
+    @Before("execution(public void add*())")
+    public void beforeAllAddMethods() {
+        System.out.println("\n======>>> Executing @Before advice on add+any name()");
     }
 }
